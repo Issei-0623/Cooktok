0a8bc2c9b8288d0a70c92046760d7714ec7d140e
issei <proteinebimayoduu@gmail.com>
2025-10-20 06:18:54 +0900

    クリックアクションでメニュー開閉実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,25 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", () => {
+  const plusBtn = document.querySelector(".plus-btn");
+  const menu = document.querySelector(".avatar-menu");
+
+  if (plusBtn && menu) {
+
+    plusBtn.addEventListener("click", (event) => {
+      event.stopPropagation();
+      menu.classList.toggle("active");
+    });
+
+    menu.addEventListener("click", (event) => {
+      event.stopPropagation();
+    });
+
+    document.addEventListener("click", () => {
+      menu.classList.remove("active");
+    });
+  }
+});
+
