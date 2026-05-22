1de354d536a18e3bbaa68a72e19ac167fae2985f
issei <proteinebimayoduu@gmail.com>
2026-05-23 08:25:06 +0900

    UIのリファクタリングと非同期処理の改善

--- app/controllers/saved_videos_controller.rb
+++ app/controllers/saved_videos_controller.rb
@@ -27,14 +27,52 @@ class SavedVideosController < ApplicationController
       @saved_video.needs_sorting = true
       @saved_video.save!
     end
-    redirect_back fallback_location: searches_path,
-                  notice: "動画を保存しました"
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "save-area-#{params[:youtube_video_id]}",
+          partial: "shared/save_button",
+          locals: {
+            video_id: params[:youtube_video_id],
+            saved: @saved_video,
+            title: @saved_video.title,
+            nickname: @saved_video.nickname,
+            username: @saved_video.username
+          }
+        )
+      end
+      format.html { redirect_back fallback_location: searches_path, notice: "動画を保存しました" }
+    end
   end
 
   def destroy
     @saved_video = current_user.saved_videos.find(params[:id])
+    youtube_video_id = params[:youtube_video_id]
+    card_dom_id      = "saved_video_#{@saved_video.id}"
+    title            = @saved_video.title
+    nickname         = @saved_video.nickname
+    username         = @saved_video.username
     @saved_video.destroy
-    redirect_back fallback_location: searches_path, notice: "保存を解除しました"
+    respond_to do |format|
+      format.turbo_stream do
+        if youtube_video_id.present?
+          render turbo_stream: turbo_stream.replace(
+            "save-area-#{youtube_video_id}",
+            partial: "shared/save_button",
+            locals: {
+              video_id: youtube_video_id,
+              saved: nil,
+              title: title,
+              nickname: nickname,
+              username: username
+            }
+          )
+        else
+          render turbo_stream: turbo_stream.remove(card_dom_id)
+        end
+      end
+      format.html { redirect_back fallback_location: searches_path, notice: "保存を解除しました" }
+    end
   end
 
   def remove_from_folder
@@ -43,6 +81,10 @@ class SavedVideosController < ApplicationController
           .where(folder_id: params[:folder_id])
           .destroy_all
     @video.update!(needs_sorting: true)
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove("saved_video_#{@video.id}") }
+      format.html { redirect_back fallback_location: folders_path, notice: "フォルダから外しました" }
+    end
   end
 
   private
--- app/javascript/homepage_foods.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("turbo:load", () => {
-  console.log("🏠 homepage_foods.js loaded");
-
-  const images = document.querySelectorAll(".food");
-  if (images.length === 0) return;
-
-  FoodBackground.applyInitialSettings(images);
-  setInterval(() => FoodBackground.replaceRandomFoods(images), 4000);
-});
